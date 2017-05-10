24829c06681e887141e0090637f39afeae638853
xaipo <xaipoj@hotmail.com>
2017-05-10 17:49:00 -0500

    agrego back de usuario completo para construir para los demas

--- pekelandia/HttpServices/Routers/apiUsuarios.js
+++ pekelandia/HttpServices/Routers/apiUsuarios.js
@@ -13,7 +13,9 @@ router.post('/login',function(req,res){
     var str='SELECT * from usuarios where user="'+req.body.user+'"and contrasenia="'+req.body.password+'"';
 
     connection.query(str, function (error, results, fields) {
-        if (error) throw error;
+        if (error) {
+            res.send(error);
+        }
         res.send(results);
     });
 
@@ -26,6 +28,25 @@ router.post('/login',function(req,res){
 
 router.post('/saveUsuario',function(req,res){
 
+    var connection = mysql.createConnection(datos);
+    var str='INSERT INTO usuarios (USER,CONTRASENIA)VALUES("'+req.body.user+'", "'+req.body.password+'")';
+    console.log(str);
+    try{
+    connection.query(str, function (error, results, fields) {
+        if (error) {
+            res.send(error);
+        }
+        res.send(results);
+
+    });
+
+    connection.end();
+
+}catch(err){
+
+
+}
+
 
 });
 
@@ -33,17 +54,73 @@ router.post('/saveUsuario',function(req,res){
 router.post('/updateUsuario',function(req,res){
 
 
+
+    var connection = mysql.createConnection(datos);
+    var str='UPDATE usuarios SET USER="'+req.body.user+'",CONTRASENIA="'+req.body.password+'" where CODIGO='+req.body.codigo;
+    console.log(str);
+    try{
+        connection.query(str, function (error, results, fields) {
+            if (error) {
+                res.send(error);
+            }
+            res.send(results);
+
+        });
+
+        connection.end();
+
+    }catch(err){
+
+
+    }
+
 });
 
 router.post('/getByIdUsuario',function(req,res){
 
 
+    var connection = mysql.createConnection(datos);
+    var str='SELECT * FROM USUARIOS where CODIGO='+req.body.codigo;
+    console.log(str);
+    try{
+        connection.query(str, function (error, results, fields) {
+            if (error) {
+                res.send(error);
+            }
+            res.send(results);
+
+        });
+
+        connection.end();
+
+    }catch(err){
+
+
+    }
 });
 
 
-router.get('/getAllUsuario',function(req,res){
+router.post('/getAllUsuario',function(req,res){
 
 
+    var connection = mysql.createConnection(datos);
+    var str='SELECT * FROM USUARIOS';
+    console.log(str);
+    try{
+        connection.query(str, function (error, results, fields) {
+            if (error) {
+                res.send(error);
+            }
+            res.send(results);
+
+        });
+
+        connection.end();
+
+    }catch(err){
+
+
+    }
 });
 
 
